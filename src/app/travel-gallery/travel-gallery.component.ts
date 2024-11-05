ef390ce2d1ccfe7557d13073d708cba28410a770
Pujan <kotharipujan123@gmail.com>
2024-11-05 15:37:53 -0500

    added css for image containers
    Added image files and image container, images to be stores under assests/images/

--- src/app/travel-gallery/travel-gallery.component.ts
+++ src/app/travel-gallery/travel-gallery.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit, } from '@angular/core';
 import { GetTravelDestinationService } from '../get-travel-destination.service';
 import { CommonModule } from '@angular/common'; // Import CommonModule for *ngIf and *ngFor
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
 @Component({
   selector: 'app-travel-gallery',
   standalone: true,
-  imports: [CommonModule], // Add CommonModule to imports to use *ngIf and *ngFor
+  imports: [CommonModule,MatProgressSpinnerModule], // Add CommonModule to imports to use *ngIf and *ngFor
   templateUrl: './travel-gallery.component.html',
   styleUrls: ['./travel-gallery.component.scss']
 })
 export class TravelGalleryComponent implements OnInit {
   destinations: any[] = []; // Update to make it explicitly an array of any type
-
+  loading=true
+  errorMsg=""
   constructor(private getTravelDestinationService: GetTravelDestinationService) {}
 
   ngOnInit(): void {
@@ -20,11 +22,15 @@ export class TravelGalleryComponent implements OnInit {
         console.log(data)
         this.destinations = data;
         
+        
       },
       error: (error) => {
+        this.loading = false;
+        this.errorMsg="Unable to load destinations. Please try again later.";
         console.error("Error fetching data from destination:", error);
       },
       complete: () => {
+        this.loading=false
         console.log("Data fetching complete.");
       }
     });
